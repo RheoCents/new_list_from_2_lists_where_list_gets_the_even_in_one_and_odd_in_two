354164385bc0aa7f71d3e20b271977b95237d0a1
unknown <centinaraphael0157@gmail.com>
2024-01-23 23:25:53 +0800

    trying some more things

--- new_list_from_2_lists_where_list_gets_the_even_in_one_and_odd_in_two.py
+++ new_list_from_2_lists_where_list_gets_the_even_in_one_and_odd_in_two.py
@@ -1,8 +1,23 @@
 #Given two list of numbers, write a program to create a new list such that 
 #the new list should contain odd numbers from the first list and even numbers from the second list.
-given_list_1 = [10, 20, 25, 30, 35]
-given_list_2 = [40, 45, 60, 75, 90]
+given_list_1 = list()
+given_list_1.append(10)
+given_list_1.append(20)
+given_list_1.append(25)
+given_list_1.append(30)
+given_list_1.append(35)
 
-list_add = str(given_list_1) + str(given_list_2)
-print(list_add)
+given_list_2 = list()
+given_list_2.append(40)
+given_list_2.append(45)
+given_list_2.append(60)
+given_list_2.append(75)
+given_list_2.append(90)
+
+len_list_of_all = list()
+len_list_of_all.append(given_list_1)
+len_list_of_all.append(given_list_2)
+
+print(len_list_of_all)
+print(len(len_list_of_all))
     
